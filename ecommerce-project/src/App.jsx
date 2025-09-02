0b7dc19d2c71845ebce794b6c91843462abbd349
kritikadhakal07 <kritikadhakal211@gmail.com>
2025-09-02 18:03:35 +0545

    Split header into a component and use Link

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -3,13 +3,15 @@ import Homepage from "./pages/HomePage.jsx";
 import CheckoutPages from "./pages/CheckoutPages.jsx";
 import OrderPages from "./pages/OrderPages.jsx";
 import TrackingPage from "./pages/TrackingPage.jsx";
-
+import Header from "./components/Header.jsx";
 function App() {
   return (
     <>
     <Routes>
       {/* <Route path = "/" element={<Homepage />}></Route> */}
+
       <Route index element={<Homepage />} />
+      
       <Route path="checkout" element={<CheckoutPages />} />
       <Route path="orders" element={<OrderPages />} />
       <Route path="tracking" element={<TrackingPage />} />
