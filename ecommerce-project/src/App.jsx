e88138ffe9335ee8a6c3ab8a531f2725ef48ad0f
kritikadhakal07 <kritikadhakal211@gmail.com>
2025-09-02 18:13:30 +0545

    Rearranged the checkout component into seperate folder

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router";
 import Homepage from "./pages/HomePage.jsx";
-import CheckoutPages from "./pages/CheckoutPages.jsx";
+import CheckoutPages from "./pages/checkout/CheckoutPages.jsx";
 import OrderPages from "./pages/OrderPages.jsx";
 import TrackingPage from "./pages/TrackingPage.jsx";
 import Header from "./components/Header.jsx";
